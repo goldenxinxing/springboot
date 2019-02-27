c01f9650df913a6e2ecffe62adc993e51a0e19d5
gaoxinxing <gaoxinxing@jd.com>
2019-02-27 15:41:55 +0800

    init

---
+++ src/main/java/foo/TranslationCommands.java
@@ -0,0 +1,30 @@
+package foo;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.Locale;
+
+@ShellComponent
+public class TranslationCommands {
+
+    //private final TranslationService service;
+
+    /*@Autowired
+    public TranslationCommands(TranslationService service) {
+        this.service = service;
+    }*/
+
+    @ShellMethod("Translate text from one language to another.")
+    public String translate(
+            @ShellOption() String text,
+            @ShellOption(defaultValue = "en_US") Locale from,
+            @ShellOption() Locale to
+    ) {
+        // invoke service
+        //        return service.translate(text, from, to);
+        System.out.println(String.format("%s,%s,%s", text, from, to));
+        return "";
+    }
+}
