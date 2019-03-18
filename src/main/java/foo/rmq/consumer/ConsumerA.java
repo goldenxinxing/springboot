147ad78a5df02ed429ca0ea6a43c8191e4053a42
gaoxinxing <gaoxinxing@megvii.com>
2019-03-18 10:08:22 +0800

    basic.*

--- src/main/java/foo/rmq/consumer/ConsumerA.java
+++ src/main/java/foo/rmq/consumer/ConsumerA.java
@@ -35,16 +35,16 @@ public class ConsumerA {
             //throw new RuntimeException("test");
         }
 
-        try {
-            //消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息
-            channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
-            //ack返回false，并重新回到队列，api里面解释得很清楚
-            channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
-            //拒绝消息
-            channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            //消息的标识，false只确认当前一个消息收到，true确认所有consumer获得的消息
+//            //channel.basicAck(message.getMessageProperties().getDeliveryTag(), false);
+//            //ack返回false，并重新回到队列，api里面解释得很清楚
+//            //channel.basicNack(message.getMessageProperties().getDeliveryTag(), false, true);
+//            //拒绝消息
+//            //channel.basicReject(message.getMessageProperties().getDeliveryTag(), true);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
         System.out.println(String.format("clean camera %d :end", cameraId));
     }
